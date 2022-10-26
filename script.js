045c278180221b9d5047ad6851f99d8b0499c953
Darksiend <logosmk2@gmail.com>
2022-10-26 19:31:17 +0300

    Working on input

---
+++ script.js
@@ -0,0 +1,6 @@
+let inputField = inputField.addEventListener("input", function () {
+  if (inputField.value[0] != inputField.value[0].toUpperCase()) {
+    (inputField.value = inputField.value.slice(0, 1).toUpperCase()) +
+      inputField.value.slice(1, inputField.value.length);
+  }
+});
