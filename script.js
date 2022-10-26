6aa9595ccf700f2eb5e07ec119180ed23c708c35
Darksiend <logosmk2@gmail.com>
2022-10-26 19:46:50 +0300

    Work

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let inputField = inputField.addEventListener("input", function () {
+let inputField = document.getElementById("input-city");
+
+inputField.addEventListener("input", function () {
   if (inputField.value[0] != inputField.value[0].toUpperCase()) {
     (inputField.value = inputField.value.slice(0, 1).toUpperCase()) +
       inputField.value.slice(1, inputField.value.length);
   }
 });
+
+let APIWeatherKey = "abe55f95b925a7dd5653ef7f8147bd6b";
