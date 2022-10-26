c848f51c3252b931999cbb66734fff2e7d12ce07
Darksiend <logosmk2@gmail.com>
2022-10-26 22:18:27 +0300

    Added Fetch Weather and City

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let inputField = document.getElementById("input-city");
-
+let inpuCityForm = document.getElementById("input-city-form");
 inputField.addEventListener("input", function () {
   if (inputField.value[0] != inputField.value[0].toUpperCase()) {
     (inputField.value = inputField.value.slice(0, 1).toUpperCase()) +
@@ -7,14 +7,21 @@ inputField.addEventListener("input", function () {
   }
 });
 
+inpuCityForm.addEventListener("submit", getCity);
+
 const APIWeatherKey = "abe55f95b925a7dd5653ef7f8147bd6b";
 
 //let curWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIWeatherKey}`;
-
-function getCity() {}
-
+let city = "";
+function getCity(event) {
+  event.preventDefault(event);
+  city = inputField.value;
+  console.log(`City: ${city}`);
+  getWeather();
+}
+let lat = 0;
+let lon = 0;
 function getWeather() {
-  let city = "london";
   fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIWeatherKey}`
   )
@@ -22,8 +29,26 @@ function getWeather() {
     .then(function (data) {
       console.log(data);
       document.getElementById("city-name").innerHTML = data[0].name;
+      lat = data[0].lat;
+
+      lon = data[0].lon;
+      getCurrentWeather().catch(function () {});
+    })
+    .then()
+    .catch(function () {});
+}
+
+function getCurrentWeather() {
+  console.log("Gettin Current Weather");
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIWeatherKey}&units=metric`
+  )
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+      document.getElementById("current-temp").innerHTML = data.main.temp + "°C";
     })
     .catch(function () {});
 }
 
-getWeather();
+//getWeather();
